fe13f6a93d11b16901d1c35a3df0c7fa778732cf
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2023-10-19 21:01:42 +0300

    changes-feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"; 
+import { useState } from "react"; 
 import { Section } from "./Section/Section";
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Statistics } from "./Statistics/Statistics";
@@ -8,20 +8,27 @@ import { MainContainer } from "./MainContainer/MainContainer.styled";
 export const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    const [bad, setBad] = useState(0)    
 
-const options = ['good', 'neutral', 'bad'];
+const options =['good', 'neutral', 'bad'];
 
-const onLeaveFeedback = (option) => {  
-    if (option === "good") {
-        setGood(good + 1);
-    } else if (option === "neutral") {
-        setNeutral(neutral + 1);
-    } else if (option === "bad") {
-        setBad(bad + 1);
-    }
+const onLeaveFeedback = (option) => { 
+    switch (option) {
+      case 'good':
+        setGood(prevState => (prevState + 1));
+        break;
+      case 'neutral':
+        setNeutral(prevState => (prevState + 1));
+        break;
+      case 'bad': 
+        setBad(prevState => (prevState + 1));
+        break;    
+      default: ; 
+    }; 
 };
+
 const total = good + neutral + bad; 
+
 const countPositiveFeedbackPercentage = () => {   
     const positivePercentage = Math.round((good/total) * 100 );
     return good ? positivePercentage : 0;      
